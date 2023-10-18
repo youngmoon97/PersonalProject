8962b4075b6e65bba6dd8931bf0c5b0565abda3f
whdudans12 <whdudans12@gmail.com>
2023-10-18 17:02:22 +0900

    2023.10.18
    vuetify project init

---
+++ personal-work/src/layouts/default/AppBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app-bar flat>
+    <v-app-bar-title>
+      <v-spacer></v-spacer>
+      <v-icon>mdi-mixed-martial-arts</v-icon>
+
+      Honest JJS
+    </v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script setup>
+//
+</script>
---
+++ personal-work/src/views/TestView.vue
@@ -0,0 +1,7 @@
+<template>
+  <Test />
+</template>
+
+<script setup>
+import Test from "@/components/Test.vue";
+</script>
